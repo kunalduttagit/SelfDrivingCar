49685df925bd7bb71d9bbe892d41ee5d382205d9
kunalduttagit <95156803+kunalduttagit@users.noreply.github.com>
2022-12-17 22:53:22 +0530

    Added Traffic

--- controls.js
+++ controls.js
@@ -1,11 +1,17 @@
 class Controls{
-    constructor(){
+    constructor(type){
         this.forward = false;
         this.left = false;
         this.right = false;
         this.reverse = false;
-
-        this.#addKeyboardListeners(); //# for private method
+        switch(type){
+            case "KEYS": 
+                this.#addKeyboardListeners(); //# for private method
+                break;
+            case "DUMMY": 
+                this.forward = true;
+                break;
+        }
     }
 
     #addKeyboardListeners(){
--- main.js
+++ main.js
@@ -3,18 +3,30 @@ myCanvas.width = 200;
 
 const ctx = myCanvas.getContext("2d");
 const road = new Road(myCanvas.width/2, myCanvas.width*0.9); //0.9 to move the lines a little closer to center, padding outside
-const car = new Car(road.getLaneCenter(1), 100, 30, 50);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS");
+const traffic = [
+    new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)
+]
 
 animate();
 
 function animate() {
-    car.update(road.borders);
+    for(let i = 0; i < traffic.length; i++) {
+        traffic[i].update(road.borders, [])
+    }
+    car.update(road.borders, traffic);
 
     myCanvas.height = window.innerHeight; //this helps to resize the canvas 60fps according to broswer length, also it resets the properties of canvas so no need ot use onClear() and onResize() function to do it every frame
     
+    ctx.save();
     ctx.translate(0, -car.y+myCanvas.height*0.7) //dont translate canvas in x axis, translate in car's y obj, negative , and place it 70% from total height of canvas
-    road.draw(ctx);
-    car.draw(ctx);
     
+    road.draw(ctx);
+    for(let i = 0; i < traffic.length; i++){
+        traffic[i].draw(ctx, "red");
+    }
+    car.draw(ctx, "blue");
+    
+    ctx.restore();
     requestAnimationFrame(animate); //reccursively call fn animate() 60 FPS/timesPS
 }
